d3afdcde4b49a84614524f8fba5586945d747d37
Bruce-Li7 <lijiaqi7788@gmail.com>
2021-09-17 20:26:59 +1000

    Add api function to user login

--- public/env.js
+++ public/env.js
@@ -1,16 +1,31 @@
 const adduser = (user) => {
     $.ajax({
         url: '/api/users',
-        data: user,
+        contentType: "application/json",
+        data: JSON.stringify(user),
         type: 'POST',
         success: (result) => {
             alert(result.message);
             location.reload();
+            alert("User created Successfully")
         }
     })
 }
 
-$(document).ready(function(){
-   
+const newUser = () => {
+    let email = $("#email").val();
+    let password = $("#pasword").val();
+    let make = $("#make").val();
+    let model = $("#model").val();
+    let year = $("#year").val();
+
+    let user = { email, password, make, model, year };
+    console.log(user);
+    adduser(user);
+}
+
+$(document).ready(function () {
+
     $('.modal').modal();
-  });
+    $('select').formSelect();
+});
