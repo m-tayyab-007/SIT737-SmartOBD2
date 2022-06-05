66ad29fda4f3820b725959506ac0976c36366db3
Miaoxuan Han <hanmiaoxuan@gmail.com>
2022-06-06 02:28:43 +1000

    update appid credentials

--- config/passport.js
+++ config/passport.js
@@ -6,8 +6,8 @@ const WebAppStrategy = require("ibmcloud-appid").WebAppStrategy;
 module.exports = function (passport) {
   passport.use(new WebAppStrategy({
     tenantId: "0371dfb0-43b0-4e63-8e5a-654cd51031ff",
-    clientId: "0755c040-798b-405c-97f3-f01b211cf1cc",
-    secret: "MTdjNjhjYWYtNjNkNS00ZTIwLTg5N2YtZWNmZTJiMDBiNDJm",
+    clientId: "febf7d32-f1a2-4a8f-954b-46c6b860b1a1",
+    secret: "NTRlMzViNDMtZjNhYy00MTYyLWI2NjYtMDEwYTQ5YjViZTRi",
     oauthServerUrl: "https://au-syd.appid.cloud.ibm.com/oauth/v4/0371dfb0-43b0-4e63-8e5a-654cd51031ff",
     redirectUri: "http://smartobd2-cloud.au-syd.mybluemix.net/ibm/cloud/appid/callback"
     }));
